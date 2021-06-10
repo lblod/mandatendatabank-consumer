d6fcc3528691727c59291b645a21818bbed9ea06
Claire Lovisa <lovisa.claire@gmail.com>
2021-06-10 13:33:38 +0200

    Add intiial sync

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import requestPromise from 'request-promise';
 import { app, errorHandler } from 'mu';
-import { INGEST_INTERVAL } from './config';
+import { INGEST_INTERVAL, SYNC_BASE_URL, STATUS_SUCCESS, STATUS_BUSY } from './config';
 import { getNextSyncTask, getRunningSyncTask, scheduleSyncTask, setTaskFailedStatus } from './lib/sync-task';
+import { scheduleInitialSyncTask } from './lib/initial-sync-task';
+import { getLatestInitialSyncJob, scheduleInitialSyncJob } from './lib/initial-sync-job';
 import { getUnconsumedFiles } from './lib/delta-file';
+import { getLatestDumpFile } from './lib/dump-file';
 import { waitForDatabase } from './lib/database-utils';
 
 /**
@@ -16,9 +19,14 @@ import { waitForDatabase } from './lib/database-utils';
 */
 
 async function triggerIngest() {
-  console.log(`Executing scheduled function at ${new Date().toISOString()}`);
+  console.log(`Executing scheduled ingest at ${new Date().toISOString()}`);
   requestPromise.post('http://localhost/ingest/');
   setTimeout( triggerIngest, INGEST_INTERVAL );
+}
+
+async function triggerInitialSync() {
+  console.log(`Executing scheduled initial sync at ${new Date().toISOString()}`);
+  requestPromise.post('http://localhost/initial-sync/');
 }
 
 waitForDatabase(async () => {
@@ -27,7 +35,7 @@ waitForDatabase(async () => {
     console.log(`Task <${runningTask.uri.value}> is still ongoing at startup. Updating its status to failed.`);
     await setTaskFailedStatus(runningTask.uri.value);
   }
-  triggerIngest();
+  triggerInitialSync();
 });
 
 app.post('/ingest', async function( req, res, next ) {
@@ -57,6 +65,52 @@ app.post('/ingest', async function( req, res, next ) {
   } else {
     console.log('A sync task is already running. A new task is scheduled and will start when the previous task finishes');
     return res.status(409).end();
+  }
+});
+
+app.post('/initial-sync', async function( req, res ) {
+  const previousJob = getLatestInitialSyncJob();
+  let job = null;
+
+  if (previousJob) {
+    if (previousJob.status == STATUS_SUCCESS) {
+      console.log(`Previous initial sync job <${previousJob.uri}> was a success, starting the ingestion of deltas.`);
+      triggerIngest();
+    } else {
+      if (previousJob.status == STATUS_BUSY) {
+        console.log(`Previous job ${previousJob.uri} seems to be stuck on busy, failing it.`);
+        previousJob.closeWithFailure();
+      }
+      console.log(`Latest previous job didn't end up as expected. Schedueling a new one.`);
+      job = await scheduleInitialSyncJob();
+    }
+  } else {
+    console.log(`No previous job found, schedueling one.`);
+    job = await scheduleInitialSyncJob();
+  }
+
+  if (job) {
+    const task = await scheduleInitialSyncTask(job);
+    console.log(`Start initial sync with ${SYNC_BASE_URL}`);
+    try {
+      const dumpFile = await getLatestDumpFile();
+      task.dumpFile = dumpFile;
+      task.execute();
+      job.updateStatus(STATUS_SUCCESS);
+      console.log(`Initial sync went smoothly, starting ingesting delta files.`);
+      triggerIngest();
+      return res.status(202).end();
+    } catch(e) {
+      console.log(`Something went wrong while doing the initial sync. Closing task with failure state.`);
+      console.trace(e);
+      await task.closeWithFailure(e);
+      await job.closeWithFailure();
+      console.log(`Error stored, stopping the service as the initial sync is mandatory.`);
+      process.exit();
+    }
+  } else {
+    console.log(`No scheduled initial sync task found.`);
+    return res.status(200).end();
   }
 });
 
--- config.js
+++ config.js
@@ -1,17 +1,46 @@
-const INGEST_INTERVAL = process.env.INGEST_INTERVAL_MS || 60000;
-const MU_APPLICATION_GRAPH = process.env.MU_APPLICATION_GRAPH || 'http://mu.semte.ch/application';
-const SYNC_BASE_URL = process.env.SYNC_BASE_URL || 'https://api.loket.lblod.info';
-const SYNC_FILES_PATH = process.env.SYNC_FILES_PATH || '/sync/mandatarissen/files';
-const SYNC_FILES_ENDPOINT = `${SYNC_BASE_URL}${SYNC_FILES_PATH}`;
-const DOWNLOAD_FILE_PATH = process.env.DOWNLOAD_FILE_PATH || '/files/:id/download';
-const DOWNLOAD_FILE_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_FILE_PATH}`;
-const BATCH_SIZE = parseInt(process.env.BATCH_SIZE) || 100;
+export const INGEST_INTERVAL = process.env.INGEST_INTERVAL_MS || 60000;
+export const MU_APPLICATION_GRAPH = process.env.MU_APPLICATION_GRAPH || 'http://mu.semte.ch/application';
+export const SYNC_BASE_URL = process.env.SYNC_BASE_URL || 'https://api.loket.lblod.info';
+export const SYNC_FILES_PATH = process.env.SYNC_FILES_PATH || '/sync/mandatarissen/files';
+export const SYNC_FILES_ENDPOINT = `${SYNC_BASE_URL}${SYNC_FILES_PATH}`;
+export const SYNC_DATASET_PATH = process.env.SYNC_DATASET_PATH || '/datasets';
+export const SYNC_DATASET_ENDPOINT = `${SYNC_BASE_URL}${SYNC_DATASET_PATH}`;
+export const DOWNLOAD_FILE_PATH = process.env.DOWNLOAD_FILE_PATH || '/files/:id/download';
+export const DOWNLOAD_FILE_ENDPOINT = `${SYNC_BASE_URL}${DOWNLOAD_FILE_PATH}`;
+export const BATCH_SIZE = parseInt(process.env.BATCH_SIZE) || 100;
+export const JOBS_GRAPH = process.env.JOBS_GRAPH || 'http://mu.semte.ch/graphs/system/jobs';
 
-export {
-  INGEST_INTERVAL,
-  MU_APPLICATION_GRAPH,
-  SYNC_BASE_URL,
-  SYNC_FILES_ENDPOINT,
-  DOWNLOAD_FILE_ENDPOINT,
-  BATCH_SIZE
-}
+if(!process.env.JOB_CREATOR_URI)
+  throw `Expected 'JOB_CREATOR_URI' to be provided.`;
+export const JOB_CREATOR_URI = process.env.JOB_CREATOR_URI;
+
+if(!process.env.INITIAL_SYNC_JOB_OPERATION)
+  throw `Expected 'INITIAL_SYNC_JOB_OPERATION' to be provided.`;
+export const INITIAL_SYNC_JOB_OPERATION = process.env.INITIAL_SYNC_JOB_OPERATION;
+
+export const INITIAL_SYNC_TASK_OPERATION = 'http://redpencil.data.gift/id/jobs/concept/TaskOperation/deltas/initialSyncing';
+export const JOB_URI_PREFIX = 'http://redpencil.data.gift/id/job/';
+export const TASK_URI_PREFIX = 'http://redpencil.data.gift/id/task/';
+export const JOB_TYPE = 'http://vocab.deri.ie/cogs#Job';
+export const TASK_TYPE = 'http://redpencil.data.gift/vocabularies/tasks/Task';
+export const ERROR_TYPE= 'http://open-services.net/ns/core#Error';
+export const DELTA_ERROR_TYPE = 'http://redpencil.data.gift/vocabularies/deltas/Error';
+export const ERROR_URI_PREFIX = 'http://redpencil.data.gift/id/jobs/error/';
+
+export const STATUS_BUSY = 'http://redpencil.data.gift/id/concept/JobStatus/busy';
+export const STATUS_SCHEDULED = 'http://redpencil.data.gift/id/concept/JobStatus/scheduled';
+export const STATUS_FAILED = 'http://redpencil.data.gift/id/concept/JobStatus/failed';
+export const STATUS_CANCELED = 'http://redpencil.data.gift/id/concept/JobStatus/canceled';
+export const STATUS_SUCCESS = 'http://redpencil.data.gift/id/concept/JobStatus/success';
+
+export const PREFIXES = `
+  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX task: <http://redpencil.data.gift/vocabularies/tasks/>
+  PREFIX dct: <http://purl.org/dc/terms/>
+  PREFIX prov: <http://www.w3.org/ns/prov#>
+  PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+  PREFIX oslc: <http://open-services.net/ns/core#>
+  PREFIX cogs: <http://vocab.deri.ie/cogs#>
+  PREFIX adms: <http://www.w3.org/ns/adms#>
+`;
